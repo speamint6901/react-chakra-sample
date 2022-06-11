d7a1f842f90ed7809648700d407bfe89fedbc64d
speamint6901 <speamint6901@gmail.com>
2022-06-11 14:25:54 +0900

    UserCardコンポーネント作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,17 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー設定</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="speamint6901"
+          fullName="junichi nasu"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
